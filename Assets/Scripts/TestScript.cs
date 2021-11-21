1d3bd3eddf0a76c03046f766b503365b2f40a9eb
David Mayfield <davidmayfield1818@yahoo.com>
2021-11-21 05:57:20 -0800

    my test commit

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This is a test script for the purpuse of making sure i can commit
+// If anyone wants to test thier commit add a there name below:
+// David Mayfield
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
